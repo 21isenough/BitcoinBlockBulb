13dce16a215b032ae00cdc892f2c57cfbf1619c7
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-15 20:10:12 +0000

    add main()

--- app.py
+++ app.py
@@ -7,3 +7,25 @@ iftttkey = "kw4Iuargdss0Sl5p-cXKB"
 debugfile = "/var/lib/docker/volumes/generated_bitcoin_datadir/_data/debug.log"
 
 tailcommand = "tail -1 " + debugfile + " | awk '{printf $5}'"
+
+
+def main():
+    block = subprocess.check_output(tailcommand, shell=True).decode("utf-8")
+    block = block[7:]
+    while True:
+
+        blocknow = subprocess.check_output(tailcommand, shell=True).decode("utf-8")
+
+        if "height" in blocknow:
+            blocknow = blocknow[7:]
+
+            if (int(block) + 1) == int(blocknow):
+                now = datetime.now()
+                date = now.strftime("%d.%m.%Y %H:%M:%S")
+                block = blocknow
+                print(block + " - New Block! (" + str(date) + ")")
+                lampcommand("new_block")
+                time.sleep(2)
+                lampcommand("turn_off")
+
+        time.sleep(1)
