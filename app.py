4437e77cba467a13f7c3d83dc6eb0fa25ed6f3f9
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-15 20:09:51 +0000

    variables

--- app.py
+++ app.py
@@ -2,3 +2,8 @@
 
 import requests, sys, subprocess, time
 from datetime import datetime
+
+iftttkey = "kw4Iuargdss0Sl5p-cXKB"
+debugfile = "/var/lib/docker/volumes/generated_bitcoin_datadir/_data/debug.log"
+
+tailcommand = "tail -1 " + debugfile + " | awk '{printf $5}'"
